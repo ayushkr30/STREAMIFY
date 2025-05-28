7bf76e0a5f96f9fd4ea4a277f5e095b3b3dd2308
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-05-28 17:54:02 +0530

    login, logout controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
 import  authRoutes  from "./routes/auth.route.js";
 import { connectDB } from "../config/db.js";
 dotenv.config();
@@ -8,6 +12,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+app.use(cors({ origin: "http://localhost:5001", credentials: true}));
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 
