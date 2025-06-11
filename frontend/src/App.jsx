2b18e03db7898927abeaad006f9ea9ea106d332d
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-06-11 23:26:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       Hello world!
       <div>
-        ncnwvsvwjjavaknc,avlancakncanwkavlvna
+        
       </div>
     </h1>
   )
